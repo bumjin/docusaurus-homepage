c1f8d77c446c729fc7d9bcea05e623e4917d3f9d
bumjin <bumjin@gmail.com>
2022-04-14 00:07:40 +0900

    chage config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://bumjin.github.io',
-  baseUrl: '/',
+  baseUrl: '/docusaurus-homepage',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
